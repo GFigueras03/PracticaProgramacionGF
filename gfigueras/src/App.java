1da170e87c9e0d888a91471a397f89ef70a134a2
GFigueras03 <guillermo.figuerasjimenez@tiernoparla.es>
2022-11-07 14:02:12 +0100

    Create java project

---
+++ gfigueras/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("ESTE SERA EL REPOSITORIO PARA LA PRACTICA DE JAVA");
+        
+    }
+}
