decf5780ffd551a8f91f8689457162797232d31d
GFigueras03 <guillermo.figuerasjimenez@tiernoparla.es>
2022-11-08 17:04:37 +0100

    Add Function to convert decimal yo binary

--- gfigueras/src/App.java
+++ gfigueras/src/App.java
@@ -1,6 +1,12 @@
+import java.util.Arrays;
+import javax.sound.sampled.SourceDataLine;
+import java.util.stream.Stream;
+
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("ESTE SERA EL REPOSITORIO PARA LA PRACTICA DE JAVA");
-        
+        //NumeroBinario.numeroEscogido = (int)Math.round(Math.random()*100000);
+        NumeroBinario.numeroEscogido = 943;
+        System.out.println(Arrays.toString(NumeroBinario.imprimirNumeroBinario()));
     }
 }
