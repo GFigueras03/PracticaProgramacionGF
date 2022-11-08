96cb14b68bcfc9ce682e391b57f02f733fe9526c
GFigueras03 <guillermo.figuerasjimenez@tiernoparla.es>
2022-11-08 15:33:47 +0100

    Create Binary to Decimal function

--- gfigueras/src/App.java
+++ gfigueras/src/App.java
@@ -1,6 +1,15 @@
+import java.util.Arrays;
+import javax.sound.sampled.SourceDataLine;
+import java.util.stream.Stream;
+
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("ESTE SERA EL REPOSITORIO PARA LA PRACTICA DE JAVA");
-        
+        NumeroBinario.numeroEscogido = (int)Math.round(Math.random()*100000);
+    //    NumeroBinario.numeroEscogido = 1000000000;
+       System.out.println(Arrays.toString(NumeroBinario.imprimirNumeroBinario()));
+       System.out.println("\n"+NumeroBinario.binarioDecimal());
+    NumeroBinario.longitudBloque();
     }
+
 }
---
+++ gfigueras/src/NumeroBinario.java
@@ -0,0 +1,56 @@
+import java.util.Arrays;
+import java.util.*;
+import java.util.stream.Stream;
+
+public class NumeroBinario{
+    public static Integer numeroEscogido = 0;
+    //!FUNCION PASAR DECIMAL A BINARIO
+    public static Integer[] imprimirNumeroBinario(){
+        String numBinario = Integer.toBinaryString(numeroEscogido);
+        Integer numerosBinarios[] = new Integer[numBinario.length()];
+        for(int i = 0; i< numBinario.length(); i++){
+          numerosBinarios[i] = Integer.parseInt(numBinario.substring(i,i+1));
+        }//for
+        return numerosBinarios;
+    }//Function
+
+    public static ArrayList<Integer> arrayList = new ArrayList<Integer>();
+    //!FUNCION PASAR BINARIO A DECIMAL
+    public static Integer binarioDecimal(){
+      Integer numeroEscogido = NumeroBinario.numeroEscogido;
+      Integer tamaño = Integer.toBinaryString(numeroEscogido).length();
+      Integer suma = 0;
+      for(int i = tamaño -1; i >= 0; i--){
+        arrayList.add(i);
+      }//for
+      System.out.println(arrayList);
+      for(int i = 0; i <= tamaño - 1; i++){
+        if(imprimirNumeroBinario()[i] != 0){
+          System.out.print((int)Math.pow(2, arrayList.get(i)) + " ");
+          suma += (int)Math.pow(2, arrayList.get(i));
+        }//if
+      }//for
+      return suma;
+      }//function
+      
+      public static ArrayList<Integer> potenciasDeDos = new ArrayList<Integer>();
+
+      public static void longitudBloque(){
+        Integer count = 0;
+        for(int i = 0; i<100; i++){
+          potenciasDeDos.add((int)(Math.pow(2, i)));
+        }
+        // System.out.println(potenciasDeDos);
+        for(int i = 0; i< arrayList.size(); i++){
+          for(int j = 0; j<potenciasDeDos.size(); j++){
+            if(i == potenciasDeDos.get(j)){
+              count++;
+            }
+          }
+        }
+        System.out.println(count);
+      }
+    }//clase
+
+  
+     
