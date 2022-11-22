21e97e5d3a8683fbf0073ef27760253ecc565d1b
GFigueras03 <guillermo.figuerasjimenez@tiernoparla.es>
2022-11-22 01:11:02 +0100

    deslimitar numero inicial

--- gfigueras/src/App.java
+++ gfigueras/src/App.java
@@ -3,18 +3,17 @@ import java.util.Arrays;
 public class App {
     public static void main(String[] args) throws Exception {
         // ?1
-        NumeroBinario.tamaño();
         System.out.println(Arrays.toString(NumeroBinario.mensaje()));
         // //!CALCULAR LONGITUD CODIGO HAMMING
-        NumeroBinario.longitudBloque();
+        // NumeroBinario.longitudBloque();
         // !
         // !CALCULAR CANTIDAD DE PARIDAD
         NumeroBinario.calcularParidad();
         // !
         // !RELLENAR EL ARRAY
         NumeroBinario.rellenarArray();
-        // // !
-        // // ! CALCULAR TODAS LAS PARIDADES Y BITS
+        // !
+        // ! CALCULAR TODAS LAS PARIDADES Y BITS
         // NumeroBinario.paridadUno();
         // NumeroBinario.paridadDos();
         // NumeroBinario.paridadTres();
